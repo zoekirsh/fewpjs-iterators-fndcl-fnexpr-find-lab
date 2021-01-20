fa9c5305d0bc9ea5c40f095a6417868703ba912b
Zoe Kirsh <zoekirsh@gmail.com>
2021-01-19 19:52:38 -0800

    green wall

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(superbowls) {
+  let winRecord = superbowls.find(superbowl => {return superbowl.result === "W"})
+  if(typeof winRecord != "undefined")
+    return winRecord.year
+}
